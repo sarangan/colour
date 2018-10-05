3568038ee78291b4ec92a0b74458470040e77194
ksara <ksara@mimaglksara8881.users.sphinet.com.sg>
2018-10-05 18:10:55 +0800

    fixing textfull search:

--- catalog/controller/information/textbookfullset.php
+++ catalog/controller/information/textbookfullset.php
@@ -9,6 +9,10 @@ class ControllerInformationTextbookfullset extends Controller {
         //     $this->response->redirect($this->url->link('account/login', '', 'SSL'));
         // }
 
+        $this->load->model('catalog/textbookfullset');
+        $this->load->model('tool/image');
+        // $this->load->model('catalog/product');
+
         $this->document->setTitle($this->language->get('heading_title'));
 
         $data['heading_title'] = 'Textbook Full Set';
@@ -17,12 +21,132 @@ class ControllerInformationTextbookfullset extends Controller {
 
         $data['heading_title'] = 'Textbook Full Set';
 
+        $data['action'] = $this->url->link('information/textbookfullset');
+
         $data['column_left'] = $this->load->controller('common/column_left');
   			$data['column_right'] = $this->load->controller('common/column_right');
   			$data['content_top'] = $this->load->controller('common/content_top');
   			$data['content_bottom'] = $this->load->controller('common/content_bottom');
   			$data['footer'] = $this->load->controller('common/footer');
   			$data['header'] = $this->load->controller('common/header');
+
+
+        $results_schools = $this->model_catalog_textbookfullset->getSchools();
+
+        $data['schools']= array();
+
+  			foreach ($results_schools as $result_school) {
+          $data['schools'][] = array(
+            'filter_id'       => $result_school['filter_id'],
+            'filter_group_id' => $result_school['filter_group_id'],
+    				'name'            => $result_school['name']
+          );
+        }
+
+        $results_levels = $this->model_catalog_textbookfullset->getLevels();
+
+        $data['levels']= array();
+
+  			foreach ($results_levels as $results_level) {
+          $data['levels'][] = array(
+            'filter_id'     => $results_level['filter_id'],
+            'filter_group_id'     => $results_level['filter_group_id'],
+    				'name'          => $results_level['name']
+          );
+        }
+
+
+        $results_streams = $this->model_catalog_textbookfullset->getStreams();
+
+        $data['streams']= array();
+
+  			foreach ($results_streams as $results_stream) {
+          $data['streams'][] = array(
+            'filter_id'     => $results_stream['filter_id'],
+            'filter_group_id'     => $results_stream['filter_group_id'],
+    				'name'          => $results_stream['name']
+          );
+        }
+
+
+        $input_school = '';
+        $input_level = '';
+        $input_stream = '';
+
+        if (isset($this->request->post['input-search-school'])) {
+    			$input_school = $this->request->post['input-search-school'];
+    		}
+
+        if (isset($this->request->post['input-search-level'])) {
+    			$input_level = $this->request->post['input-search-level'];
+    		}
+
+        if (isset($this->request->post['input-search-stream'])) {
+    			$input_stream = $this->request->post['input-search-stream'];
+    		}
+
+        if( $input_level == '---' ){
+          $input_level = '';
+        }
+
+        if( $input_stream == '---' ){
+          $input_stream = '';
+        }
+
+        $data['input_school'] = $input_school;
+        $data['input_level'] = $input_level;
+        $data['input_stream'] = $input_stream;
+
+        $data['products'] = array();
+        
+        $results = $this->model_catalog_textbookfullset->getProducts($input_school, $input_level, $input_stream);
+
+        foreach ($results as $result) {
+    			if ($result['image']) {
+    				$image = $this->model_tool_image->resize($result['image'], $this->config->get($this->config->get('config_theme') . '_image_product_width'), $this->config->get($this->config->get('config_theme') . '_image_product_height'));
+    			} else {
+    				$image = $this->model_tool_image->resize('placeholder.png', $this->config->get($this->config->get('config_theme') . '_image_product_width'), $this->config->get($this->config->get('config_theme') . '_image_product_height'));
+    			}
+
+    			if ($this->customer->isLogged() || !$this->config->get('config_customer_price')) {
+    				$price = $this->currency->format($this->tax->calculate($result['price'], $result['tax_class_id'], $this->config->get('config_tax')), $this->session->data['currency']);
+    			} else {
+    				$price = false;
+    			}
+
+    			if ((float)$result['special']) {
+    				$special = $this->currency->format($this->tax->calculate($result['special'], $result['tax_class_id'], $this->config->get('config_tax')), $this->session->data['currency']);
+    			} else {
+    				$special = false;
+    			}
+
+    			if ($this->config->get('config_tax')) {
+    				$tax = $this->currency->format((float)$result['special'] ? $result['special'] : $result['price'], $this->session->data['currency']);
+    			} else {
+    				$tax = false;
+    			}
+
+    			if ($this->config->get('config_review_status')) {
+    				$rating = (int)$result['rating'];
+    			} else {
+    				$rating = false;
+    			}
+
+    			$data['products'][] = array(
+    				'product_id'  => $result['product_id'],
+    				'thumb'       => $image,
+    				'name'        => $result['name'],
+    				'description' => utf8_substr(strip_tags(html_entity_decode($result['description'], ENT_QUOTES, 'UTF-8')), 0, $this->config->get($this->config->get('config_theme') . '_product_description_length')) . '..',
+    				'price'       => $price,
+    				'special'     => $special,
+    				'tax'         => $tax,
+    				'minimum'     => $result['minimum'] > 0 ? $result['minimum'] : 1,
+    				'rating'      => $result['rating'],
+    				'date_added'  => $result['date_added'],
+            'publisher'  => $result['mpn'],
+    				'href'        => $this->url->link('product/product', 'product_id=' . $result['product_id'] . $url)
+    			);
+    		}
 
 
         $this->response->setOutput($this->load->view('information/textbookfullset', $data));
--- catalog/view/theme/default/template/information/textbookfullset.tpl
+++ catalog/view/theme/default/template/information/textbookfullset.tpl
@@ -11,10 +11,127 @@
     <div id="content" class="<?php echo $class; ?>"><?php echo $content_top; ?>
       <h1 class="category-top-title"><?php echo $heading_title; ?></h1>
 
-        <p>Hi man</p>
+
+      <form action="<?php echo $action; ?>" method="post" accept-charset="utf-8" enctype="multipart/form-data" class="form-horizontal">
+        <div class="row div-extra-wrapper">
+
+          <div class="col-sm-1">
+            <strong>School</strong>
+          </div>
+
+          <div class="col-sm-6">
+              <select name="input-search-school" id="input-search-school" class="form-control no-radious">
+                <?php
+                   foreach ($schools as $school) { ?>
+                      <option value="<?php echo $school['filter_id']; ?>" <?php echo $school['filter_id'] == $input_school ? 'selected="selected"' : ''; ?>><?php echo $school['name']; ?>
+                      </option>
+                <?php } ?>
+              </select>
+          </div>
+
+        </div>
+
+        <div class="row div-extra-wrapper">
+
+          <div class="col-sm-1">
+            <strong>Levels</strong>
+          </div>
+
+          <div class="col-sm-6">
+              <select name="input-search-level" id="input-search-level" class="form-control no-radious">
+                <option value="---">---</option>
+                <?php
+                   foreach ($levels as $level) { ?>
+                      <option value="<?php echo $level['filter_id']; ?>" <?php echo $level['filter_id'] == $input_level ? 'selected="selected"' : ''; ?>><?php echo $level['name']; ?>
+                      </option>
+                <?php } ?>
+              </select>
+          </div>
+
+        </div>
+
+        <div class="row div-extra-wrapper">
+
+          <div class="col-sm-1">
+            <strong>Streams</strong>
+          </div>
+
+          <div class="col-sm-6">
+              <select name="input-search-stream" id="input-search-stream" class="form-control no-radious" >
+                <option value="---">---</option>
+                <?php
+                   foreach ($streams as $stream) { ?>
+                      <option value="<?php echo $stream['filter_id']; ?>" <?php echo $stream['filter_id'] == $input_stream ? 'selected="selected"' : ''; ?>><?php echo $stream['name']; ?>
+                      </option>
+                <?php } ?>
+              </select>
+          </div>
+
+        </div>
+
+        <div class="row div-extra-wrapper">
+          <div class="col-sm-1">
+          </div>
+          <div class="col-sm-3">
+            <button type="submit" class="button-colour" style="width: 100%; padding: 7px;">Search</button>
+          </div>
+
+        </div>
+
+      </form>
+
+
+      <div class="row div-extra-wrapper">
+
+        <table class="table">
+
+          <thead>
+            <tr>
+              <td><strong>Select all</strong></td>
+              <td><strong>Subject</strong></td>
+              <td><strong>Tittle of book</strong></td>
+              <td><strong>Publisher</strong></td>
+              <td><strong>Price</strong></td>
+            </tr>
+          </thead>
+
+          <tbody>
+
+            <?php foreach ($products as $product) { ?>
+              <tr>
+                <td>check</td>
+                <td><?php echo $product['jan']; ?></td>
+                <td><?php echo $product['location']; ?></td>
+                <td><?php echo $product['name']; ?></td>
+                <td><?php echo $product['mpn']; ?></td>
+                <td>
+                <?php if ($product['price']) { ?>
+                <p class="price">
+                  <?php if (!$product['special']) { ?>
+                  <span class="overlay-product-price"><?php echo $product['price']; ?></span>
+                  <?php } else { ?>
+                  <span class="overlay-product-price"><?php echo $product['special']; ?></span> <span class="price-old"><?php echo $product['price']; ?></span>
+                  <?php } ?>
+                  <?php if ($product['tax']) { ?>
+                  <span class="overlay-product-price"><?php echo $text_tax; ?> <?php echo $product['tax']; ?></span>
+                  <?php } ?>
+                </p>
+                <?php } ?>
+                <td>
+              </tr>
+            <?php } ?>
+          </tbody>
+  
+        </table>
+
+
+      </div>
+
+
 
       <?php echo $content_bottom; ?>
     </div>
-    <?php echo $column_right; ?></div>
+    <?php echo $column_right; ?>
+  </div>
 </div>
 <?php echo $footer; ?>
